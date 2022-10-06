8a9ef5271ecab110e5df27ec8fba79e5f7eeaa67
BasO12 <94169344+BasO12@users.noreply.github.com>
2022-10-06 09:39:02 +0200

    Add files via upload

---
+++ python_kamerskiezen_verbeterd.py
@@ -0,0 +1,61 @@
+import tkinter as tk
+from tkinter import *
+from tkcalendar import Calendar, DateEntry
+
+#define on button and check which room is chosen
+
+def on_button():
+    for i in l.curselection():
+        if OPTIONS[i] == "Kamer 1":
+            print("gekozen voor kamer 1")
+            root.destroy()
+            datepick()
+        elif OPTIONS[i] == "Kamer 2":
+            print("gekozen voor kamer 2")
+            root.destroy()
+            datepick()
+        elif OPTIONS[i] == "Kamer 3":
+            print("gekozen voor kamer 3")
+            root.destroy()
+            datepick()
+    print()
+
+def datepick():
+    def datechoice():
+        print(cal.get_date())
+    win= tk.Tk()
+    #Set the Geometry
+    win.geometry("250x250")
+    win.title("Date Picker")
+    #Create a Label
+    Label(win, text= "Choose a Date", background= 'gray61', foreground="white").pack(padx=20,pady=20)
+    #Create a Calendar using DateEntry
+    cal = DateEntry(win, width= 16, background= "magenta3", foreground= "white",bd=2)
+    cal.pack(pady=20)
+    b = tk.Button(win, text='OK', command=datechoice)
+    b.pack(fill='x')
+    win.mainloop()
+
+
+# --- tkinter list and menu ---
+
+OPTIONS = ["Kamer 1", "Kamer 2", "Kamer 3"]
+
+root = tk.Tk()
+root.geometry("500x200")
+root.title("kamerkiezer")
+
+# --- Listbox ---
+
+tk.Label(root, text='Kamers', bg='#2da3b3').pack(fill='x')
+
+l = tk.Listbox(root, selectmode='single', bg='#2da3b3')
+l.pack()
+l.insert('end', *OPTIONS)
+
+# --- ok button and call function ---
+
+b = tk.Button(root, text='OK', command=on_button)
+b.pack(fill='x')
+
+root.mainloop()
